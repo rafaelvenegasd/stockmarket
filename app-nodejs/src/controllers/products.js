c060bbd12af74725430c252901b7da03c3465fbd
Marcelo Gaspar <drmpgaspar@gmail.com>
2020-03-27 03:42:56 +0100

    Test and use Chart.js in app

--- app-nodejs/src/controllers/products.js
+++ app-nodejs/src/controllers/products.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
-router.get('/', (req, res) => {
-    res.render('pages/products');
+router.get('/', (req, res, next) => {
+    axios
+    .get("http://localhost:3000/companies?_page=1&_limit=5")
+    .then((response) => {
+        let companiesArray = [];
+        response.data.map((products)=>{
+            companiesArray.push(products);
+        });
+        res.render('pages/products', { companies: companiesArray });  
+    })
+    .catch(err => console.error(err)) 
+});
+
+
+router.get(`/:id`, (req, res, next) => {
+    axios
+    .get(`http://localhost:3000/companies?id=1`)
+    .then((response) => {
+        let companiesArray = [];
+        response.data.map((item)=>{
+            companiesArray.push(item);
+        });
+        res.render('pages/item', { companies: companiesArray });  
+    })
+    .catch(err => console.error(err)) 
 });
 
 module.exports = router;
